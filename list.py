74da9997df6080f1ef98884d343d788f286db97c
kyle6193 <kyle.transue@gmail.com>
2025-04-15 10:02:43 -0400

    Added small tutorial on list making

---
+++ list.py
@@ -0,0 +1,12 @@
+friends = ["ricky", "chris", "kevin", "madi", "april"]
+print(friends [1])
+print("\n")
+mathfriends = [8*43, "kyle", "andrew", "roger"]
+print(mathfriends[0])
+print("\n")
+multi = [["margo", "1234 annangale road", 3348695], ["sara", "84323 river road", 7834995], ["jake", "781308 brentdale ct", 1289463]]
+print(multi[1])
+print("\n")
+print(multi[2][2])
+
+# lists always start at 0. Even nested lists.
